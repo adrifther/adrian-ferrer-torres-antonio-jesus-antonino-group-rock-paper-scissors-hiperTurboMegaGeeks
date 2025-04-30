d815eac8faa826f5c61205516d969d68302dfc9b
Adri <adrianferrert@gmail.com>
2025-04-30 18:25:10 +0200

    Update app.js
    eliminacion de comentarios y de espacios y saltos de parrafo, indentación corregida, console.logs eliminados

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ import "./style.css";
 import "./assets/img/4geeks.ico";
 import { Button } from "bootstrap";
 
-
-  
 const _randomize = (anArray) => {
   const randomNumber = Math.floor(Math.random()*anArray.length);
     const wordInstance = anArray[randomNumber];
@@ -19,73 +17,51 @@ const lizard = {name : 'lizard', enemyList : ['scissors', 'rock']};
 const spock = {name : 'spock', enemyList : ['paper', 'lizard']}; 
 
 const userSelects = (id) => {
-  //Podemos mirar refactor
-  let selectedId = '';
     if(id == rock.name) {
-      console.log('user chose rock');
       document.getElementById('user-selection').innerText = 'rock';
-      selectedId = 'rock';
       return rock; 
     }
     if(id == paper.name) {
-      console.log('user chose paper');
       document.getElementById('user-selection').innerText = 'paper';
-      selectedId = 'paper';
       return paper;
     }
     if(id == scissors.name) {
-      console.log('user chose scissors');
       document.getElementById('user-selection').innerText = 'scissors';
-      selectedId = 'scissors';
       return scissors;
     }
     if(id == lizard.name) {
-      console.log('user chose lizard');
       document.getElementById('user-selection').innerText = 'lizard';
-      selectedId = 'lizard';
       return lizard;
     }
     if(id == spock.name) {
-      console.log('user chose spock');
       document.getElementById('user-selection').innerText = 'spock';
-      selectedId = 'spock';
       return spock;
     };
 }; 
   
 function playGame(userSelection){
-  
   const possibleSelections = [ rock , paper , scissors , lizard , spock];
   const pcSelector = () =>  _randomize(possibleSelections);
   const pcSelection = pcSelector().name;
-
   let isInEnemyList = function(userSelects){
     let enemyList =  userSelects.enemyList;
       for(let enemy of enemyList){
         if(pcSelection === enemy) {return true}; 
-        
       }
       return false; 
   } ; 
-  
-    document.getElementById('pc-selection').innerText = `${pcSelection}`;
-    if(pcSelection === userSelection) {
+  document.getElementById('pc-selection').innerText = `${pcSelection}`;
+  if(pcSelection === userSelection) {
       console.log('draw');
       return document.getElementById('result').innerText = 'DRAW';
-      
-    }
+  }
+  const userSelectionEnemyList = userSelection.enemyList;
 
-    const userSelectionEnemyList = userSelection.enemyList;
-    console.log(userSelection.enemyList);
-
-    if(isInEnemyList(userSelection)){
-        return document.getElementById('result').innerText = 'PC WINS you lose ';
-    }
-    {
-        return document.getElementById('result').innerText = 'USER WINS';
-        }
-
-    }
+  if(isInEnemyList(userSelection)){
+      return document.getElementById('result').innerText = 'PC WINS you lose ';
+  }
+  {return document.getElementById('result').innerText = 'USER WINS';}
+}
         
   document.getElementById('rock').addEventListener('click', () => {playGame( userSelects('rock'))});
   document.getElementById('paper').addEventListener('click', () => {playGame( userSelects('paper'))});
